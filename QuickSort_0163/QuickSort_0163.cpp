506ddda5a5a1ce5aeb451a0fd7bb63f072a43eab
naxsyambis <naxsyambis@gmail.com>
2024-03-28 11:22:38 +0000

    membuat for

--- QuickSort_0163/QuickSort_0163.cpp
+++ QuickSort_0163/QuickSort_0163.cpp
@@ -89,7 +89,10 @@ void display() {
     cout << "Sorted Array" << endl;
     cout << "--------------------" << endl;
 
-
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
 int main()
 {
