a99528317125c47a32044931b5aebf9293eadfd2
naxsyambis <naxsyambis@gmail.com>
2024-03-28 11:01:44 +0000

    membuat prosedur swap

--- QuickSort_0163/QuickSort_0163.cpp
+++ QuickSort_0163/QuickSort_0163.cpp
@@ -29,6 +29,16 @@ void input() {
     }
 }
 
+// Menukar elemen pada indeks x dengan elemen pada indeks y
+void swap(int x, int y)
+{
+    int temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+    mov_count++;
+}
+
+
 
 
 int main()
