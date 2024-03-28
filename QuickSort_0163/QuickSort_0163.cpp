ed0b8c01802fb935bae9bd363ebd1e1d79787184
naxsyambis <naxsyambis@gmail.com>
2024-03-28 11:03:15 +0000

    membuat prosedur q_sort dengan variabel low dan high)

--- QuickSort_0163/QuickSort_0163.cpp
+++ QuickSort_0163/QuickSort_0163.cpp
@@ -38,7 +38,13 @@ void swap(int x, int y)
     mov_count++;
 }
 
-
+void q_sort(int low, int high)
+{
+    int temp;
+    int pivot, i, j;
+    if (low > high) { // step 1
+        return;
+    }
 
 
 int main()
