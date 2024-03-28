099f5e3868a9b25c82fdf26bdb235ba8df6ab8a7
naxsyambis <naxsyambis@gmail.com>
2024-03-28 10:21:07 +0000

    membuat array bilangan bulat untuk menyimpan nilai

--- QuickSort_0163/QuickSort_0163.cpp
+++ QuickSort_0163/QuickSort_0163.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// array bilangan bulat untuk menyimpan nilai
+int arr[20];
+int cmp_count = 0; // jumlah perbandingan
+int mov_count = 0; // jumlah pergerakan data
+int n;
+
+
 int main()
 {
     
